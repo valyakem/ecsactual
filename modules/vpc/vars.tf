832a78ace85480f6017300b93427e5fe6502eb0c
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-17 14:53:10 -0400

    added some other files

--- modules/vpc/vars.tf
+++ modules/vpc/vars.tf
@@ -1,36 +1,3 @@
-variable "name" {
-  description = "the name of your stack, e.g. \"demo\""
-}
-
-variable "environment" {
-  description = "the name of your environment, e.g. \"prod\""
-}
-
-variable "cidr" {
-  description = "The CIDR block for the VPC."
-  default = "10.0.0.0/16"
-}
-
-variable "public_subnets" {
-  description = "List of public subnets"
-}
-
-variable "private_subnets" {
-  description = "List of private subnets"
-}
-
-variable "availability_zones" {
-  description = "List of availability zones"
-}
-
-variable "name" {
-  description = "the name of your stack, e.g. \"demo\""
-}
-
-variable "environment" {
-  description = "the name of your environment, e.g. \"prod\""
-}
-
 variable "cidr" {
   description = "The CIDR block for the VPC."
   default = "10.0.0.0/16"
