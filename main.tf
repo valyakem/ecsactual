9ab8dcde8b32dbbf609c1533d1e96ebc16dfa601
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-17 19:11:08 -0400

    Added some now

--- main.tf
+++ main.tf
@@ -46,30 +46,30 @@ module "vpc" {
   environment        = var.environment
 }
 
-module "security_groups" {
-  source         = "./modules/sec-group"
-  name           = var.name
-  vpc_id         = module.vpc.id
-  environment    = var.environment
-  container_port = var.container_port
-}
+# module "security_groups" {
+#   source         = "./modules/sec-group"
+#   name           = var.name
+#   vpc_id         = module.vpc.id
+#   environment    = var.environment
+#   container_port = var.container_port
+# }
 
-module "alb" {
-  source              = "./modules/alb"
-  name                = var.name
-  vpc_id              = module.vpc.id
-  subnets             = module.vpc.public_subnets
-  environment         = var.environment
-  alb_security_groups = [module.security_groups.alb]
-  alb_tls_cert_arn    = var.tsl_certificate_arn
-  health_check_path   = var.health_check_path
-}
+# module "alb" {
+#   source              = "./modules/alb"
+#   name                = var.name
+#   vpc_id              = module.vpc.id
+#   subnets             = module.vpc.public_subnets
+#   environment         = var.environment
+#   alb_security_groups = [module.security_groups.alb]
+#   alb_tls_cert_arn    = var.tsl_certificate_arn
+#   health_check_path   = var.health_check_path
+# }
 
-module "ecr" {
-  source      = "./modules/ecr"
-  name        = var.name
-  environment = var.environment
-}
+# module "ecr" {
+#   source      = "./modules/ecr"
+#   name        = var.name
+#   environment = var.environment
+# }
 
 
 # module "secrets" {
@@ -79,26 +79,26 @@ module "ecr" {
 #   application-secrets = var.application-secrets
 # }
 
-module "ecs" {
-  source                      = "./modules/ecs"
-  name                        = var.name
-  environment                 = var.environment
-  region                      = var.aws-region
-  subnets                     = module.vpc.private_subnets
-  aws_alb_target_group_arn    = module.alb.aws_alb_target_group_arn
-  ecs_service_security_groups = [module.security_groups.ecs_tasks]
-  container_port              = var.container_port
-  container_cpu               = var.container_cpu
-  container_memory            = var.container_memory
-  service_desired_count       = var.service_desired_count
-  container_environment = [
-    { name = "LOG_LEVEL",
-    value = "DEBUG" },
-    { name = "PORT",
-    value = var.container_port }
-  ]
-  # container_secrets      = module.secrets.secrets_map
-  aws_ecr_repository_url = module.uri_repo
-  # container_secrets_arns = module.secrets.application_secrets_arn
-}
+# module "ecs" {
+#   source                      = "./modules/ecs"
+#   name                        = var.name
+#   environment                 = var.environment
+#   region                      = var.aws-region
+#   subnets                     = module.vpc.private_subnets
+#   aws_alb_target_group_arn    = module.alb.aws_alb_target_group_arn
+#   ecs_service_security_groups = [module.security_groups.ecs_tasks]
+#   container_port              = var.container_port
+#   container_cpu               = var.container_cpu
+#   container_memory            = var.container_memory
+#   service_desired_count       = var.service_desired_count
+#   container_environment = [
+#     { name = "LOG_LEVEL",
+#     value = "DEBUG" },
+#     { name = "PORT",
+#     value = var.container_port }
+#   ]
+#   # container_secrets      = module.secrets.secrets_map
+#   aws_ecr_repository_url = module.uri_repo
+#   # container_secrets_arns = module.secrets.application_secrets_arn
+# }
 
