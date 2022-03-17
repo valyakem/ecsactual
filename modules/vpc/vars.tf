88fc11e20f984d192b5e87810fbd019e51f76dc4
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-17 13:38:56 -0400

    My first commit

---
+++ modules/vpc/vars.tf
@@ -0,0 +1,53 @@
+variable "name" {
+  description = "the name of your stack, e.g. \"demo\""
+}
+
+variable "environment" {
+  description = "the name of your environment, e.g. \"prod\""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC."
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+}
+
+variable "private_subnets" {
+  description = "List of private subnets"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+}
+
+variable "cidr" {
+  type    = string
+  default = "145.0.0.0/16"
+}
+
+variable "azs" {
+  type = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+}
+
+variable "subnets-ip" {
+  type = list(string)
+  default = [
+    "145.0.1.0/24",
+    "145.0.2.0/24"
+  ]
+}
+
+variable "public_subnets" {
+  description = "List of public subnets"
+  type = list(string)
+  default = [
+    "10.0.16.0/20", 
+    "10.0.48.0/20"
+  ]
+}
---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "repo_name" {
+  type    = string
+  default = "testecr"
+}
+
+variable "branch_name" {
+  type    = string
+  default = "main"
+}
+
+variable "build_project" {
+  type    = string
+  default = "dev-build-repo"
+}
+
+variable "uri_repo" {
+  type = string
+  default ="440153443065.dkr.ecr.us-east-1.amazonaws.com/testecr"
+}
+
+variable "tsl_certificate_arn" {
+  type = string
+  default ="mycertificatearn"
+}
