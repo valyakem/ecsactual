88450e4c6442c5153c9173e958a8dfdc51b55e75
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-17 18:48:04 -0400

    Update some cool

--- modules/ecr/vars.tf
+++ modules/ecr/vars.tf
@@ -1,12 +1,12 @@
-variable "name" {
-  description = "the name of your stack, e.g. \"demo\""
-   default = "pricing-tool"
-}
+# variable "name" {
+#   description = "the name of your stack, e.g. \"demo\""
+#    default = "pricing-tool"
+# }
 
-variable "environment" {
-  description = "the name of your environment, e.g. \"prod\""
-   default     = "prod"
-}
+# variable "environment" {
+#   description = "the name of your environment, e.g. \"prod\""
+#    default     = "prod"
+# }
 
 variable "uri_repo" {
   type = string
