25ab07f40fa3be0d61a9a6e10d4bc238574a20ed
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-17 21:05:57 -0400

    Added more values

--- vars.tf
+++ vars.tf
@@ -37,10 +37,6 @@ variable "availability_zones" {
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
-variable "cidr" {
-  description = "The CIDR block for the VPC."
-  default     = "10.0.0.0/16"
-}
 
 variable "private_subnets" {
   description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
