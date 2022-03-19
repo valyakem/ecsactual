c697dc6fa8422415ac4495c1505770a58c5ae8e0
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-19 10:47:36 -0400

    Added some files

--- variables.tf
+++ variables.tf
@@ -680,3 +680,23 @@ variable "additional_certificates_arn_for_https_listeners" {
   type        = list(any)
   default     = []
 }
+
+variable "repo_name" {
+  type    = string
+  default = "testecr"
+}
+
+variable "branch_name" {
+  type    = string
+  default = "main"
+}
+
+variable "build_project" {
+  type    = string
+  default = "dev-build-repo"
+}
+
+variable "uri_repo" {
+  type = string
+  default ="440153443065.dkr.ecr.us-east-1.amazonaws.com/testecr"
+}
