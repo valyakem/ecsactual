aef37515ff70584ae1aac67bba61a14850d9b789
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-19 08:18:31 -0400

    Added vpc module

---
+++ modules/vars.tf
@@ -0,0 +1,26 @@
+variable "cidr" {
+  type    = string
+  default = "145.0.0.0/16"
+}
+
+variable "azs" {
+  type = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b"
+  ]
+}
+
+variable "subnets-ip" {
+  type = list(string)
+  default = [
+    "145.0.1.0/24",
+    "145.0.2.0/24"
+  ]
+
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "145.0.0.0/16"
+}
--- vars.tf
+++ vars.tf
@@ -18,27 +18,23 @@ variable "uri_repo" {
   default ="440153443065.dkr.ecr.us-east-1.amazonaws.com/testecr"
 }
 
-variable "environment" {
-  description = "the name of your environment, e.g. \"prod\""
-  default     = "prod"
-}
+# variable "environment" {
+#   description = "the name of your environment, e.g. \"prod\""
+#   default     = "prod"
+# }
 
 variable "availability_zones" {
   description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
   default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
-variable "private_subnets" {
-  description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
-  default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
-}
+# variable "private_subnets" {
+#   description = "a list of CIDRs for private subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
+#   default     = ["10.0.0.0/20", "10.0.32.0/20", "10.0.64.0/20"]
+# }
 
 variable "public_subnets" {
   description = "a list of CIDRs for public subnets in your VPC, must be set if the cidr variable is defined, needs to have as many elements as there are availability zones"
   default     = ["10.0.16.0/20", "10.0.48.0/20", "10.0.80.0/20"]
 }
 
-variable "vpc_id" {
-  type    = string
-  default = "145.0.0.0/16"
-}
