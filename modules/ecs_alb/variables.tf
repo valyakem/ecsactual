b0b903c05ea5b59d778a5ba7a1d09af6e89682e3
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-20 11:28:02 -0400

    Fixed policy issues

--- modules/ecs_alb/variables.tf
+++ modules/ecs_alb/variables.tf
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 variable "name_prefix" {
   description = "Name prefix for resources on AWS"
-  default     = "ims-pricingtool-22"
+  default     = "imspricingtool1201"
 }
 
 variable "tags" {
