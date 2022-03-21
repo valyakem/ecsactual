3cc58fe445357db96f56780c563f47b2120b537a
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-20 22:40:01 -0400

    Added

--- alb/main.tf
+++ alb/main.tf
@@ -42,31 +42,31 @@ resource "aws_alb_listener" "http" {
   port              = 80
   protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
+  # default_action {
+  #   type = "redirect"
 
-    redirect {
-      port        = 443
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
+  #   redirect {
+  #     port        = 443
+  #     protocol    = "HTTPS"
+  #     status_code = "HTTP_301"
+  #   }
+  # }
 }
 
-# # Redirect traffic to target group
-resource "aws_alb_listener" "https" {
-    load_balancer_arn = aws_lb.main.id
-    port              = 443
-    protocol          = "HTTPS"
+# # # Redirect traffic to target group
+# resource "aws_alb_listener" "https" {
+#     load_balancer_arn = aws_lb.main.id
+#     port              = 443
+#     protocol          = "HTTPS"
 
-    ssl_policy        = "ELBSecurityPolicy-2016-08"
-    # certificate_arn   = var.alb_tls_cert_arn
+#     ssl_policy        = "ELBSecurityPolicy-2016-08"
+#     # certificate_arn   = var.alb_tls_cert_arn
 
-    default_action {
-        target_group_arn = aws_alb_target_group.main.id
-        type             = "forward"
-    }
-}
+#     default_action {
+#         target_group_arn = aws_alb_target_group.main.id
+#         type             = "forward"
+#     }
+# }
 
 output "aws_alb_target_group_arn" {
   value = aws_alb_target_group.main.arn
