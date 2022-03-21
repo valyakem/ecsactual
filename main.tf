d15588ed6418612ab9849e2cb9bf00fc2de036ec
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-21 12:24:24 -0400

    Added some

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
-provider "aws" {
-  access_key = var.aws-access-key
-  secret_key = var.aws-secret-key
-  region     = var.aws-region
-  version    = "~> 2.0"
-}
+# provider "aws" {
+#   access_key = var.aws-access-key
+#   secret_key = var.aws-secret-key
+#   region     = var.aws-region
+#   version    = "~> 2.0"
+# }
 
-terraform {
-  backend "s3" {
-    bucket  = "terraform-backend-store"
-    encrypt = true
-    key     = "terraform.tfstate"
-    region  = "eu-central-1"
-    # dynamodb_table = "terraform-state-lock-dynamo" - uncomment this line once the terraform-state-lock-dynamo has been terraformed
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket  = "terraform-backend-store"
+#     encrypt = true
+#     key     = "terraform.tfstate"
+#     region  = "eu-central-1"
+#     # dynamodb_table = "terraform-state-lock-dynamo" - uncomment this line once the terraform-state-lock-dynamo has been terraformed
+#   }
+# }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "terraform-state-lock-dynamo"
-  hash_key       = "LockID"
-  read_capacity  = 20
-  write_capacity = 20
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  tags = {
-    Name = "DynamoDB Terraform State Lock Table"
-  }
-}
+# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+#   name           = "terraform-state-lock-dynamo"
+#   hash_key       = "LockID"
+#   read_capacity  = 20
+#   write_capacity = 20
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+#   tags = {
+#     Name = "DynamoDB Terraform State Lock Table"
+#   }
+# }
 
 module "vpc" {
   source             = "./vpc"
