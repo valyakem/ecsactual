0c4fc7453de0cc028e51d2898679e053a5f23d2e
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-21 09:09:27 -0400

    added some files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -60,7 +60,7 @@ resource "aws_eip" "nat_eip" {
   )
 }
 
-# NAT Gateways
+# NAT GatewaysNat
 resource "aws_nat_gateway" "nat_gw" {
   count         = var.single_nat ? 1 : length(var.availability_zones)
   allocation_id = var.single_nat ? aws_eip.nat_eip.0.id : element(aws_eip.nat_eip.*.id, count.index)
