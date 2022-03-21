14be9f898388f9ee1aa6922da031cf083ab5e4fc
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-21 16:09:45 -0400

    Added more values

--- variables.tf
+++ variables.tf
@@ -10,27 +10,33 @@ variable "enable_dns_hostnames" {
 
 variable "arcablanca_pub_subnet_1_cidr" {
   description               = "Arca-Blanca public subnet 1 cidr"
+  default                   = "10.0.1.0/24" 
 }
 
 variable "arcablanca_pub_subnet_2_cidr" {
   description               = "Arca-Blanca public subnet 2 cidr"
+  default                   = "10.0.2.0/24" 
 }
 
 variable "arcablanca_pub_subnet_3_cidr" {
   description               = "Arca-Blanca public subnet 3 cidr"
+  default                   = "10.0.3.0/24"
 }
 
 
 variable "arcablanca_private_subnet_1_cidr" {
   description               = "Arca-Blanca private subnet 1 cidr"
+  default                   = "10.0.4.0/24" 
 }
 
 
 variable "arcablanca_private_subnet_2_cidr" {
   description               = "Arca-Blanca private subnet 2 cidr"
+  default                   = "10.0.2.0/24" 
 }
 
 variable "arcablanca_private_subnet_3_cidr" {
   description               = "Arca-Blanca private subnet 3 cidr"
+  default                   = "10.0.2.0/24" 
 }
 
