d688455e2008cf0544c227938929de366812b35a
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-22 08:54:29 -0400

    Fixed certificate errors

--- domain.tf
+++ domain.tf
@@ -13,10 +13,10 @@ data "aws_route53_zone" "arca-blanca-ecsdomain" {
 }
 
 resource "aws_route53_record" "abpt_ecs_cert_validation_record" {
-    name                            = "${aws_acm_certificate.arca-blanca-domaincert.domain_validation_options.0.resource_record_name}" 
-    type                            = "${aws_acm_certificate.arca-blanca-domaincert.domain_validation_options.0.resource_record_type}"
+    name                            = "${tolist(aws_acm_certificate.arca-blanca-domaincert.domain_validation_options)[0].resource_record_name}" 
+    type                            = "${tolist(aws_acm_certificate.arca-blanca-domaincert.domain_validation_options)[0].resource_record_type}"
     zone_id                         = "${data.aws_route53.arca-blanca-ecsdomain.zone_id}" 
-    records                         = ["${aws_acm_certificate.arca-blanca-domaincert.domain_validation_options.0.resource_record_value}"]
+    records                         = ["${tolist(aws_acm_certificate.arca-blanca-domaincert.domain_validation_options)[0].resource_record_value}"]
     ttl                             = 60
 }
 
