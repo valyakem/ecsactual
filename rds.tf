55d0c44c8c05c62a9981ac22f940c45f3cf2d550
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-23 12:45:35 -0400

    Update RDS SG

--- rds.tf
+++ rds.tf
@@ -37,23 +37,23 @@ resource "aws_security_group" "arcablanca_rds_sg" {
   vpc_id                        = "${aws_vpc.arca-blanca-ptvpc.id}"
 
   ingress {
-    description      = "Open port 5432 default port for rds"
-    from_port        = 5432
-    to_port          = 5432
-    protocol         = "tcp"
-    cidr_blocks      = [var.vpc_id]
-  } 
+      from_port         = 5432
+      to_port           = 5432
+      protocol          = "tcp"
+      security_groups   = ["${aws_security_group.arcablanca-alb-sg.id}"]
+  }  
   ingress {
       from_port         = 5433
       to_port           = 5433
       protocol          = "tcp"
-      cidr_blocks      = [var.vpc_id]
+      security_groups = ["${aws_security_group.arcablanca-alb-sg.id}"]
   }
   egress {
-      from_port         = 0
-      to_port           = 0
-      protocol          = "tcp"
-      cidr_blocks       = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
